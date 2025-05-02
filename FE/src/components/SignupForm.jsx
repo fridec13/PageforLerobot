d5636bbb45ef2b0c021bd7409ffc3eaa91c71a29
jaemin-hand <thswoalsrns@naver.com>
2025-05-02 17:49:33 +0900

    Feat:회원가입 레이아웃 구현

---
+++ FE/src/components/SignupForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('회원가입 데이터:', formData);
+    // TODO: Spring Boot로 POST 요청 보내기
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>사용자명</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>이메일</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>비밀번호</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">회원가입</button>
+    </form>
+  );
+};
+
+export default SignupForm;
